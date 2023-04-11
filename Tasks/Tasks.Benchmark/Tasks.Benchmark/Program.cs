bfa5c21644f0ca28396b602c8f88695bc9c7588c
Guilherme Sampaio <santos.gsampaio@gmail.com>
2023-04-10 22:00:13 -0300

    Initial commit

---
+++ Tasks/Tasks.Benchmark/Tasks.Benchmark/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using Tasks.Benchmark;
+
+BenchmarkRunner.Run<WhenAllVsShortCircuit>();
+
---
+++ Tasks/Tasks.Benchmark/Tasks.Benchmark/WhenAllVsShortCircuit.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Benchmark
+{
+    [MemoryDiagnoser]
+    public class WhenAllVsShortCircuit
+    {
+        private readonly int iterations = 100;
+        private readonly int delayMilliseconds = 5;
+        private Task[] _tasks;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _tasks = new Task[iterations];
+            for (int i = 0; i < iterations; i++)
+            {
+                _tasks[i] = Task.Delay(delayMilliseconds);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task AwaitSerially()
+        {
+            for (int i = 0; i < _tasks.Length; i++)
+            {
+                await _tasks[i];
+            }
+        }
+
+
+        [Benchmark]
+        public async Task WhenAll()
+        {
+            await Task.WhenAll(_tasks);
+        }
+
+
+        [Benchmark]
+        public async Task ShortCircuit()
+        {
+            for (int i = 0; i < _tasks.Length; i++)
+            {
+                var task = _tasks[i];
+                if (task.IsCompletedSuccessfully)
+                    continue;
+                await task;
+            }
+        }
+    }
+}
